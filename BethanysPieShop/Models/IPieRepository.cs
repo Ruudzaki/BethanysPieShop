b7aac58b05b0fd412fa9bf66969271e2bfe92b80
Kostiantyn B <kostybelous@gmail.com>
2023-06-04 11:19:37 +0300

    Add project files.

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BethanysPieShop.Models;
+
+public interface IPieRepository {
+    IEnumerable<Pie> AllPies { get; }
+    IEnumerable<Pie> PiesOfTheWeek { get; }
+    Pie? GetPieById(int pieId);
+}
