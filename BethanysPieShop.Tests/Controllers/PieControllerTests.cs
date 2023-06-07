b0e09bbd6c053e2e507a1c73eebe918417ad1064
Kostiantyn B <kostybelous@gmail.com>
2023-06-07 14:42:01 +0300

    add first uts for pie controller

---
+++ BethanysPieShop.Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BethanysPieShop.Controllers;
+using BethanysPieShop.Tests.Mocks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Tests.Controllers;
+
+public class PieControllerTests {
+
+    [Fact]
+    public void List_EmptyCategory_ReturnsAllPies() {
+
+        var mockPieRepository = RepositoryMocks.GetPieRepository();
+        var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+        var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+        var result = pieController.List("");
+
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+
+        Assert.Equal(10, pieListViewModel.Pies.Count());
+    }
+}
