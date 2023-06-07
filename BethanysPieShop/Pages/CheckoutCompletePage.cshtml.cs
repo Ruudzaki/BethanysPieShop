9aa46f2669d1fe6018b649af475ecb61860afb29
Kostiantyn B <kostybelous@gmail.com>
2023-06-07 14:14:54 +0300

    add checkout as a razor page

---
+++ BethanysPieShop/Pages/CheckoutCompletePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Pages
+{
+    public class CheckoutCompletePageModel : PageModel
+    {
+        public void OnGet() {
+            ViewData["CheckoutCompleteMessage"] = "Thanks for you order. You'll soon enjoy our delicious pies!";
+        }
+    }
+}
